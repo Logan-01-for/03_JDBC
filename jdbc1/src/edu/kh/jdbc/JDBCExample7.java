f6f90c149014b5b6f20e9ff4ba81246e7034fa83
Logan-01-for <sjson0105@gmail.com>
2025-03-28 17:39:42 +0900

    0 3 2 8

--- jdbc1/src/edu/kh/jdbc/JDBCExample7.java
+++ jdbc1/src/edu/kh/jdbc/JDBCExample7.java
@@ -9,7 +9,6 @@ import java.util.Scanner;
 public class JDBCExample7 {
 
 	public static void main(String[] args) {
-		
 		// EMPLOYEE	테이블에서
 		// 사번, 이름, 성별, 급여, 직급명, 부서명을 조회
 		// 단, 입력 받은 조건에 맞는 결과만 조회하고 정렬할 것
@@ -27,109 +26,124 @@ public class JDBCExample7 {
 				
 		// 사번 | 이름   | 성별 | 급여    | 직급명 | 부서명
 		//--------------------------------------------------------
+		// 217  | 전지연 | F    | 3660000 | 대리   | 인사관리부 
+
+		// -------------------------------------------------------
+		
+		// 사번 | 이름   | 성별 | 급여    | 직급명 | 부서명
+		//--------------------------------------------------------
 		// 218  | 이오리 | F    | 3890000 | 사원   | 없음
 		// 203  | 송은희 | F    | 3800000 | 차장   | 해외영업2부
 		// 212  | 장쯔위 | F    | 3550000 | 대리   | 기술지원부
 		// 222  | 이태림 | F    | 3436240 | 대리   | 기술지원부
 		// 207  | 하이유 | F    | 3200000 | 과장   | 해외영업1부
 		// 210  | 윤은해 | F    | 3000000 | 사원   | 해외영업1부
-
+		
+		// 1. JDBC 객체 참조변수 선언
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-
+		
 		Scanner sc = null;
 		
 		try {
-			// Oracle JDBC 드라이버 로드
+			// 2. DriverManager를 이용해서 Connection 생성
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			
 			String url = "jdbc:oracle:thin:@localhost:1521:XE";
 			String userName = "kh";
 			String password = "kh1234";
 			
-			// DB 연결
-			conn = DriverManager.getConnection(url, userName,password);
+			conn = DriverManager.getConnection(url, userName, password);
 			
 			// 3. SQL 작성
-			sc = new Scanner (System.in);
-			System.out.println("조회할 성별 (M/F) : ");
+			sc = new Scanner(System.in);
+			System.out.print("조회할 성별(M/F) : ");
 			String gender = sc.next().toUpperCase();
 			
-			System.out.println("급여 범위 (최소, 최대 순서로 작성) : ");
+			System.out.println("급여 범위(최소, 최대 순서로 작성) : ");
 			int min = sc.nextInt();
 			int max = sc.nextInt();
 			
-			System.out.println("급여 정렬 (1. ASC, 2.DESC) : ");
+			System.out.print("급여 정렬(1.ASC, 2.DESC) : ");
 			int sort = sc.nextInt();
 			
-			// 사번, 이름, 성별, 급여, 직급명, 부서명을 조회
 			String sql = """
-						SELECT EMP_ID, EMP_NAME
-						DECOD(SUBSTR(EMP_NO, 8, 1) '1', 'M', '2', 'F')GENDER,
-						SALARY, JOB_NAME
-						FROM EMPLOYEE
-						WHERE SALARY BETWEEN 3000000 AND 4000000
-						ORDER BY""" + ASC
+					SELECT EMP_ID, EMP_NAME, 
+					DECODE(SUBSTR(EMP_NO, 8, 1), '1', 'M', '2', 'F') GENDER,
+					SALARY, JOB_NAME, NVL(DEPT_TITLE, '없음') DEPT_TITLE
+					FROM EMPLOYEE
+					JOIN JOB USING(JOB_CODE)
+					LEFT JOIN DEPARTMENT ON(DEPT_CODE = DEPT_ID)
+					WHERE 
+						DECODE(SUBSTR(EMP_NO, 8, 1), '1', 'M', '2', 'F') = ?
+					AND SALARY BETWEEN ? AND ?
+					ORDER BY SALARY
 					""";
-
-				// 급여의 오름차순인지 내림차순인지 조건에 따라 SQL 보완하기
-				if (sort == 1) sql += "ASC";
-				else			sql+= "DESC";
 			
-			// 4. prepareStatement 객체 생성
+			// 급여의 오름차순인지 내림차순인지 조건에 따라 sql 보완하기
+			if(sort == 1) sql += " ASC";
+			else 		  sql += " DESC";
+			
+			
+			// 4. PreparedStatement 객체 생성
 			pstmt = conn.prepareStatement(sql);
 			
-			// 5. ? (위치 홀더)에 알맞은 값 세팅
+			// 5. ? (위치홀더)에 알맞은 값 세팅
 			pstmt.setString(1, gender);
-			pstmt.setString(2, min);
-			pstmt.setString(3, max);
+			pstmt.setInt(2, min);
+			pstmt.setInt(3, max);
 			
 			// 6. SQL 수행 후 결과 반환 받기
-			rs = pstmt.executeQuery(sql);
+			rs = pstmt.executeQuery();
 			
-		// 7. 커서를 이용해서 한 행씩 접근하여 컬럼 값 얻어오기
-			System.out.printf("사번 | 이름   | 성별 | 급여    | 직급명 | 부서명");
-
-			boolean flag = true; // true : 조회 결과가 없음, false : 조회 결과 존재함!
+			// 7. 커서를 이용해서 한 행씩 접근하여 컬럼 값 얻어오기
+			System.out.println("사번 | 이름   | 성별 | 급여    | 직급명 | 부서명");
+			System.out.println("--------------------------------------------------------");
+			
+			boolean flag = true; // true : 조회결과가 없음, false : 조회결과 존재함!
 			
 			while(rs.next()) {
-				flag = false; // while 문이 1회 이상 반복함 == 조회 결과가 1행이라도 있다
-										// 컬럼명, 별칭, 조회된 컬럼 순서 가능
-				String empId = rs.getString("EMP_ID");
-				String empId = rs.getString("EMP_NAME");
-				String empId = rs.getString("GENDER");
-				int salary = rs.getInt("SALARY");
-				String jobName = rs.getString("JOB_NAME");
-				String deptTiltle = rs.getString("DEPT_TITLE");
+				flag = false; // while문이 1회 이상 반복함 == 조회결과가 1행이라도 있다
+									// 컬럼명, 별칭, 조회된컬럼순서 가능
+				String empId 	= rs.getString("EMP_ID");
+				String empName 	= rs.getString("EMP_NAME");
+				String gen 		= rs.getString("GENDER");
+				int salary 		= rs.getInt("SALARY");
+				String jobName 	= rs.getString("JOB_NAME");
+				String deptTitle = rs.getString("DEPT_TITLE");
 				
 				System.out.printf("%-4s | %3s | %-4s | %7d | %-3s  | %s \n",
 						empId, empName, gen, salary, jobName, deptTitle);
 			}
-
-			if(flag) {// flag == true 인 경우 - > while 문 안쪽 수행 X -> 조회 결과가 1행도 없음
-				System.out.println("조회 경로가 없음");
+			
+			if(flag) { // flag == true인 경우 -> while문 안쪽 수행 X -> 조회결과가 1행도 없음
+				System.out.println("조회 결과 없음");
 			}
+			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
-		
-		}finally {
-			// 8.사용한 JDBC 객체 자원 반환
+			
+		} finally {
+			// 8.  사용한 JDBC 객체 자원 반환
 			
 			try {
-				// 자원 반환
-				if (rs != null)rs.close();
-				if (pstmt != null)pstmt.close();
-				if (conn !=null) conn.close();
 				
-				if(sc !=null)sc.close();
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+				
+				if(sc != null) sc.close();
 				
 			} catch (Exception e) {
-			e.printStackTrace();
+				e.printStackTrace();
 			}
+			
+			
+			
 		}
-
+		
 	}
 
 }
